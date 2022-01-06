d040df5a7e63acb288b8cdd7c9ff098f4374ed75
jereq1 <jereq1@student.monash.edu>
2022-01-06 22:41:17 +1100

    completed road-to-react Meet Components chapter

--- road-to-react/hacker-stories/src/App.js
+++ road-to-react/hacker-stories/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'John',
+    url: 'https://www.youtube.com',
+    author: 'John Requizo',
+    num_comments: 1,
+    points: 10,
+    objectID: 0,
+  },
+];
+
+function getTitle(title) {
+  return title;
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author} </span>
+            <span>Number of comments: {item.num_comments} </span>
+            <span>{item.points} </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hello {getTitle('React')}</h1>
+
+      <Search />
+      <hr/>
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
