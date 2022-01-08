a63426d036094ce852ea08413ad170c04c66535b
jereq1 <jereq1@student.monash.edu>
2022-01-08 12:27:46 +1100

    used javascript spread and rest operator on List Component

--- road-to-react/hacker-stories/src/App.js
+++ road-to-react/hacker-stories/src/App.js
@@ -56,26 +56,26 @@ const List = ({list}) => {
 
   return (
     <ul>
-      {list.map((item) => {
+      {list.map(({objectID, ...item}) => {
         return (
-          <Item key={item.objectID} item={item} />
+          <Item key={objectID} {...item} />
         );
       })}
     </ul>
   );
 }
 
-const Item = ({item}) => {
+const Item = ({title, url, author, num_comments, points}) => {
   console.log('The Item renders');
 
   return (
     <li> 
       <span>
-        <a href={item.url}>{item.title} </a>
+        <a href={url}>{title} </a>
       </span>
-      <span>{item.author} </span>
-      <span>Number of comments: {item.num_comments} </span>
-      <span>{item.points} </span>
+      <span>{author} </span>
+      <span>Number of comments: {num_comments} </span>
+      <span>{points} </span>
     </li>
   );
 };
