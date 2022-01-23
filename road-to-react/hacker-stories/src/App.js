5d3ee1d3a9f13cc5214f538434f5903fcb20c5d7
John Requizo <jereq1@student.monash.edu>
2022-01-23 11:33:36 +1100

    completed chapter on Inline handler for road-to-react

--- road-to-react/hacker-stories/src/App.js
+++ road-to-react/hacker-stories/src/App.js
@@ -13,7 +13,7 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -42,6 +42,16 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -59,26 +69,30 @@ const App = () => {
 
       <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={handleSearch}><strong>Search:</strong> </InputWithLabel>
       <hr/>
-      <List list={searchedStories}/>
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
     </div>
   );
 }
 
-const List = ({list}) => {
+const List = ({list, onRemoveItem}) => {
   console.log('The List renders');
 
   return (
     <ul>
       {list.map((item) => {
         return (
-          <Item key={item.objectID} item={item} />
+          <Item 
+            key={item.objectID} 
+            item={item} 
+            onRemoveItem={onRemoveItem}
+          />
         );
       })}
     </ul>
   );
 }
 
-const Item = ({item}) => {
+const Item = ({item, onRemoveItem}) => {
   console.log('The Item renders');
 
   return (
@@ -89,6 +103,11 @@ const Item = ({item}) => {
       <span>{item.author} </span>
       <span>Number of comments: {item.num_comments} </span>
       <span>{item.points} </span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
     </li>
   );
 };
