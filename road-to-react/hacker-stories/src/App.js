2f39c8cfd6fb0b591549bbcfe90208b77b0c350d
John Requizo <jereq1@student.monash.edu>
2022-02-07 23:04:03 +1100

    completed chapter on react asynchronous Data

--- road-to-react/hacker-stories/src/App.js
+++ road-to-react/hacker-stories/src/App.js
@@ -40,9 +40,24 @@ const App = () => {
     },
   ];
 
+  const getAsyncStories = () => {
+    return new Promise((resolve) => {
+      setTimeout( 
+        () => resolve({data: {stories: initialStories}}), 
+        2000
+      )
+    });
+  };
+
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
